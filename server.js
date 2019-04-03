e0a2a29979a9d73e9360fd2c6ca082b57a150d56
Danish Akhtar <akhtar@Danishs-MacBook-Pro.local>
2019-04-02 22:23:45 -0400

    added create-react-app

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const logger = require("morgan");
+var cors = require('cors');
+
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Configure middleware
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve up static assets
+app.use(express.static("client/build"));
+
+// Set up a whitelist and check against it:
+// var whitelist = ['http://localhost:3001/api/'];
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
+
+// Then pass them to cors:
+// app.use(cors(corsOptions));
+
+// Enable CORS by using it before your routes. This will allow axios to trigger API calls to server port (from a diff port 3000 -> 3001) 
+app.use(cors());
+
+// Add routes
+app.use(routes);
+
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+
+//Connect to MongoDB
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://localhost/mytodolist",
+	{ useNewUrlParser: true }
+);
+
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
+// Start API Server
+app.listen(PORT, () => {
+	console.log(`🌎  ==> API Server now listening on PORT ${ PORT }`);
+});
