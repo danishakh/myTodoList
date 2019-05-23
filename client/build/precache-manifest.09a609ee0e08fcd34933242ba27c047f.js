f84b23b696dafeb283bd70a4fd1d1b694de47b31
danishakh <danishakh@gmail.com>
2019-05-23 03:09:10 -0400

    updated build server

---
+++ client/build/precache-manifest.09a609ee0e08fcd34933242ba27c047f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4aba1242ec5cb95a4090e18925f2f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa85a89fc808ef2dcfef",
+    "url": "/static/css/main.6c0c0f16.chunk.css"
+  },
+  {
+    "revision": "06156f5377029cc9156b",
+    "url": "/static/js/2.bf54d974.chunk.js"
+  },
+  {
+    "revision": "fa85a89fc808ef2dcfef",
+    "url": "/static/js/main.9f8be3e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
